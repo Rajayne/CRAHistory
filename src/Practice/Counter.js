e65bd7317317a6c1eba919ef1fb1154daf8ac112
Rajayne <ryann.j.nealey@gmail.com>
2023-05-18 15:50:37 -0700

    Create class component state and update using setState()

--- src/Practice/Counter.js
+++ src/Practice/Counter.js
@@ -3,12 +3,16 @@ import React from "react";
 class Counter extends React.Component {
   constructor(props) {
     super(props);
-    console.log(this.props);
+    this.state = { count: 0 };
   }
   render() {
     return (
       <div>
         <h1 style={{ color: this.props.color }}>Counter</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Add 1
+        </button>
       </div>
     );
   }
