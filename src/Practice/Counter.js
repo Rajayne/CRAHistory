97cf3ae3f87bf2c271c687762e4f0a9fa9a61889
Rajayne <ryann.j.nealey@gmail.com>
2023-05-18 16:00:32 -0700

    Bind functions in the constructor

--- src/Practice/Counter.js
+++ src/Practice/Counter.js
@@ -4,15 +4,24 @@ class Counter extends React.Component {
   constructor(props) {
     super(props);
     this.state = { count: 0 };
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+
+  increment() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  decrement() {
+    this.setState({ count: this.state.count - 1 });
   }
   render() {
     return (
       <div>
         <h1 style={{ color: this.props.color }}>Counter</h1>
         <p>Count: {this.state.count}</p>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-          Add 1
-        </button>
+        <button onClick={this.increment}>+ 1</button>
+        <button onClick={this.decrement}>- 1</button>
       </div>
     );
   }
