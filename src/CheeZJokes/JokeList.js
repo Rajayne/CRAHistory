a99d0ce0dc11f51f008b4863ec248a1538f8f115
Rajayne <ryann.j.nealey@gmail.com>
2023-05-18 17:32:27 -0700

    Integrate axios request and if seenJokes from ClassComponent

--- src/CheeZJokes/JokeList.js
+++ src/CheeZJokes/JokeList.js
@@ -1,11 +1,37 @@
 import React, { useState } from "react";
 import Joke from "./Joke";
+import axios from "axios";
 
 const JokeList = () => {
   const numJokes = 5;
   const [seenJokes, setSeenJokes] = useState(new Set());
   const [jokes, setJokes] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+
+  async function getJokes() {
+    try {
+      let res = await axios.get("https://icanhazdadjoke.com", {
+        headers: { Accept: "application/json" },
+      });
+
+      let joke = res.data;
+
+      setJokes({ ...joke });
+      console.log(jokes);
+
+      if (!seenJokes.has(joke.id)) {
+        seenJokes.add(joke.id);
+        jokes.push({ ...joke, votes: 0 });
+      } else {
+        console.log("duplicate found!");
+      }
+
+      setIsLoading(false);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <div>
       <h1>Joke List</h1>
@@ -17,37 +43,6 @@ const JokeList = () => {
 export default JokeList;
 
 /*
-  async getJokes() {
-    try {
-      // load jokes one at a time, adding not-yet-seen jokes
-      let jokes = [];
-      let seenJokes = new Set();
-
-      while (jokes.length < this.props.numJokesToGet) {
-        let res = await axios.get("https://icanhazdadjoke.com", {
-          headers: { Accept: "application/json" }
-        });
-        let { ...joke } = res.data;
-
-        if (!seenJokes.has(joke.id)) {
-          seenJokes.add(joke.id);
-          jokes.push({ ...joke, votes: 0 });
-        } else {
-          console.log("duplicate found!");
-        }
-      }
-
-      this.setState({ jokes, isLoading: false });
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  generateNewJokes() {
-    this.setState({ isLoading: true});
-    this.getJokes();
-  }
-
   vote(id, delta) {
     this.setState(st => ({
       jokes: st.jokes.map(j =>
